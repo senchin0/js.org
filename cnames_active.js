da6362ca4265389b825e1d924d039dddd324c26a
senchin0 <149279969+senchin0@users.noreply.github.com>
2025-04-13 09:03:18 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2901,6 +2901,7 @@ var cnames_active = {
   "selectric": "lcdsantos.github.io/jQuery-Selectric", // noCF? (don´t add this in a new PR)
   "semantic-api": "semantic-api.github.io/docs",
   "semo": "semojs.github.io/semo",
+  "senchin": "senchin0.github.io/senchin0"
   "senil": "mrfarhad.github.io/senilwebsite",
   "seoul": "seouljs.github.io/seoul.js.org",
   "sequel": "sequeljs.github.io",
